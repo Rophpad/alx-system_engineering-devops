400c6d3d5bd5d66b437865c1c0a491f538e17765
Rophpad <rophenp@gmail.com>
2024-01-16 14:09:11 +0100

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,14 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the numbers of subscribers for a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "rophpad@alx-holbertonschool"}
+    headers = {
+            "User-Agent": "rophpad"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    result = response.json().get("data")
-    return result.get("subscribers")
+    elif response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
