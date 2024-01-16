0382bd22028c0ee4a96c7ee738aacce6fd0d348f
Rophpad <rophenp@gmail.com>
2024-01-16 14:47:17 +0100

    Task 2 done

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,7 +7,7 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "Rophpad"
+        "User-Agent": "underscoDe@alx-holbertonschool"
     }
     params = {
         "after": after,
