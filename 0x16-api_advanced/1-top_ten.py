23e001719db82f706a992365ba1443733c08c8e7
Rophpad <rophenp@gmail.com>
2024-01-16 14:36:01 +0100

    Task 1 done

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Function to query Reddit API to get informations"""
+import requests
+
+
+def top_ten(subreddit):
+     """Prints the titles of the 10 hottest posts on a given subreddit"""
+     headers = {'User-Agent': 'Rophpad'}
+     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+     response = requests.get(url, headers=headers, allow_redirects=False)
+     if response.status_code == 200:
+         data = response.json()
+         if 'data' in data and 'children' in data['data']:
+             for post in data['data']['children']:
+                 print(post['data']['title'])
+         else:
+             print(None)
+     else:
+         print(None)
