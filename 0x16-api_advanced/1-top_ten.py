981582ba64ffe6524efcf41178619cebff1c4277
Rophpad <rophenp@gmail.com>
2024-01-16 14:39:29 +0100

    pycodestyle errors corrected

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,16 +4,16 @@ import requests
 
 
 def top_ten(subreddit):
-     """Prints the titles of the 10 hottest posts on a given subreddit"""
-     headers = {'User-Agent': 'Rophpad'}
-     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-     response = requests.get(url, headers=headers, allow_redirects=False)
-     if response.status_code == 200:
-         data = response.json()
-         if 'data' in data and 'children' in data['data']:
-             for post in data['data']['children']:
-                 print(post['data']['title'])
-         else:
-             print(None)
-     else:
-         print(None)
+    """Prints the titles of the 10 hottest posts on a given subreddit"""
+    headers = {'User-Agent': 'Rophpad'}
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data and 'children' in data['data']:
+            for post in data['data']['children']:
+                print(post['data']['title'])
+        else:
+            print(None)
+    else:
+        print(None)
