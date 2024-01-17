09617c5117eace188036089ae924120e4b613fbf
Rophpad <rophenp@gmail.com>
2024-01-17 12:09:36 +0100

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 """Function to query Reddit API to get informations"""
-import requests
+from requests import get
 
 
 def top_ten(subreddit):
     """Prints the titles of the 10 hottest posts on a given subreddit"""
-    headers = {'User-Agent': 'Rophpad'}
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        if 'data' in data and 'children' in data['data']:
-            for post in data['data']['children']:
-                print(post['data']['title'])
-        else:
-            print(None)
-    else:
-        print(None)
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+
+    user_agent = {'User-agent': 'My user Agent'}
+    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
+
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        my_data = results.get('data').get('children')
+
+        for i in my_data:
+            print(i.get('data').get('title'))
+
+    except Exception:
+        print("None")
