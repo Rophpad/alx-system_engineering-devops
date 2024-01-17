af354c94db7d35fff9d2532e285a9c09b4e258a7
Rophpad <rophenp@gmail.com>
2024-01-17 11:08:35 +0100

    code updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,21 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the numbers of subscribers for a given subreddit"""
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "Rophpad"
+    user_agent = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+                AppleWebKit/537.36 (KHTML, like Gecko)\
+                Chrome/120.0.0.0 Safari/537.36"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=user_agent, allow_redirects=False)
+    data = response.json()
+    try:
+        return data["data"]["subscribers"]
+    except Exception:
+        return 0
+    """
     if response.status_code == 404:
         return 0
     elif response.status_code == 200:
@@ -17,3 +27,4 @@ def number_of_subscribers(subreddit):
         return data["data"]["subscribers"]
     else:
         return 0
+    """
